07f6e62cd7b64dee6d604d0f616fda58865db366
MorningCoffeeZombie <40343762+MorningCoffeeZombie@users.noreply.github.com>
2019-01-15 16:10:06 -0600

    Should have been removed before...

--- gmod_shortcut.sh
+++
@@ -1,6 +0,0 @@
-# Garry's Mod for Linux tends to crash and have instability issues
-# Allegedly some of this is due to GMod using Internet Explorer protocols to download server files
-# For some reason GMod might actually have a chance of running when you run the following commands:
-
-cd ~/.local/share/Steam/steamapps/common/GarrysMod
-./hl2.sh
