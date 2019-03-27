79b04e8ce9dc56c24ac8e9b165c5d6d8e80e30e2
MorningCoffeeZombie <40343762+MorningCoffeeZombie@users.noreply.github.com>
2019-03-26 22:54:37 -0500

    More sounds, better script

--- ShellScripts/swapMicSpams.sh
+++ ShellScripts/swapMicSpams.sh
@@ -11,36 +11,38 @@ NC='\033[0m' # No Color
 SCRIPTLOCATION=$(pwd)
 
 # Set default game
-$GAME="Counter-Strike Global Offensive"
+GAME="Counter-Strike Global Offensive"
 
-if [ "${1,,}" = "csgo" ] || [ "${1,,}" = "" ]; then
-	$GAME="Counter-Strike Global Offensive"
-	sudo printf "GAME=Counter-Strike Global Offensive\n" | sudo tee --append swapMicSpams.conf
-elif [ "${1,,}" = "csgo" ] || [ "${1,,}" = "cs go" ]; then
-	$GAME="Counter-Strike Source"
-	sudo printf "GAME=Counter-Strike Source\n" | sudo tee --append swapMicSpams.conf
-elif [ "${1,,}" = "dod" ] || [ "${1,,}" = "dods" ]; then
-	$GAME="Day of Defeat Source"
-	sudo printf "GAME=Day of Defeat Source\n" | sudo tee --append swapMicSpams.conf
-elif [ "${1,,}" = "gmod" ] || [ "${1,,}" = "garrysmod" ]; then
-	$GAME="GarrysMod"
-	sudo printf "GAME=GarrysMod\n" | sudo tee --append swapMicSpams.conf
-elif [ "${1,,}" = "portal" ]; then
-	$GAME="Portal"
-	sudo printf "GAME=Portal\n" | sudo tee --append swapMicSpams.conf
-elif [ "${1,,}" = "l4d" ] || [[ "${1,,}" = lef* ]]; then
-	$GAME="left 4 dead"
-	sudo printf "GAME=left 4 dead\n" | sudo tee --append swapMicSpams.conf
-elif [ "${1,,}" = "tf2" ] || [[ "${1,,}" = team* ]]; then
-	$GAME="Team Fortress 2"
-	sudo printf "GAME=Team Fortress 2\n" | sudo tee --append swapMicSpams.conf
+if [[ "${1,,}" = gam* ]] || [[ "${2,,}" = mod* ]]; then
+	if [ "${2,,}" = "csgo" ] || [ "${2,,}" = "" ]; then
+		GAME="Counter-Strike Global Offensive"
+		sudo printf "GAME=Counter-Strike Global Offensive\n" | sudo tee --append swapMicSpams.conf
+	elif [ "${2,,}" = "csgo" ] || [ "${2,,}" = "cs go" ]; then
+		GAME="Counter-Strike Source"
+		sudo printf "GAME=Counter-Strike Source\n" | sudo tee --append swapMicSpams.conf
+	elif [ "${2,,}" = "dod" ] || [ "${2,,}" = "dods" ]; then
+		GAME="Day of Defeat Source"
+		sudo printf "GAME=Day of Defeat Source\n" | sudo tee --append swapMicSpams.conf
+	elif [ "${2,,}" = "gmod" ] || [ "${2,,}" = "garrysmod" ]; then
+		GAME="GarrysMod"
+		sudo printf "GAME=GarrysMod\n" | sudo tee --append swapMicSpams.conf
+	elif [ "${2,,}" = "portal" ]; then
+		GAME="Portal"
+		sudo printf "GAME=Portal\n" | sudo tee --append swapMicSpams.conf
+	elif [ "${2,,}" = "l4d" ] || [[ "${2,,}" = lef* ]]; then
+		GAME="left 4 dead"
+		sudo printf "GAME=left 4 dead\n" | sudo tee --append swapMicSpams.conf
+	elif [ "${2,,}" = "tf2" ] || [[ "${2,,}" = team* ]]; then
+		GAME="Team Fortress 2"
+		sudo printf "GAME=Team Fortress 2\n" | sudo tee --append swapMicSpams.conf
+	fi
 fi
 
 
 # Catch errors
 if [ ! -d /steamapps/common/$GAME ]; then
-	printf "${RED}GAME NOT INSTALLED AS EXPECTED!${NC}"
-	printf "${RED}EXITING SCRIPT${NC}"
+	printf "${RED}GAME NOT INSTALLED AS EXPECTED!${NC}\n"
+	printf "${RED}EXITING SCRIPT${NC}\n"
 	exit
 fi
 
@@ -51,7 +53,7 @@ do
 	echo  
 	echo  
 	PS3="Which soundbyte would you like to que? "
-	options=("Bomb Planted" "Concentrate" "Drinking Problem" "FBI Open Up" "Kinky" "Medic!" "OOF" "IM THE MOUNTAIN" "Quit")
+	options=("Bomb Planted" "Concentrate" "Drinking Problem" "FBI Open Up" "Get To Choppa" "Chris Hansen" "Medic!" "OOF" "IM THE MOUNTAIN" "Virgin In House" "Quit")
 	select voiceinput in "${options[@]}"
 	do
 	case $voiceinput in
@@ -63,16 +65,22 @@ do
 			cp $SCRIPTLOCATION/MicSpams/drinking_problem_voice_input.wav "/home/$USER/.steam/steam/steamapps/common/$GAME/voice_input.wav"; echo "${BOLDFONT}$voiceinput${NORMALFONT} entered"; break;;
 		"FBI Open Up")
 			cp $SCRIPTLOCATION/MicSpams/fbi-open-up-sfx_voice_input.wav "/home/$USER/.steam/steam/steamapps/common/$GAME/voice_input.wav"; echo "${BOLDFONT}$voiceinput${NORMALFONT} entered"; break;;
-		"Kinky")
-			cp $SCRIPTLOCATION/MicSpams/kinky_voice_input.wav "/home/$USER/.steam/steam/steamapps/common/$GAME/voice_input.wav"; echo "${BOLDFONT}$voiceinput${NORMALFONT} entered"; break;;
+		"Get To Choppa")
+			cp $SCRIPTLOCATION/MicSpams/get_to_choppa_voice_input.wav "/home/$USER/.steam/steam/steamapps/common/$GAME/voice_input.wav"; echo "${BOLDFONT}$voiceinput${NORMALFONT} entered"; break;;
+		"Chris Hansen")
+			cp $SCRIPTLOCATION/MicSpams/im_chris_hansen_voice_input.wav "/home/$USER/.steam/steam/steamapps/common/$GAME/voice_input.wav"; echo "${BOLDFONT}$voiceinput${NORMALFONT} entered"; break;;
+#		"Kinky")
+#			cp $SCRIPTLOCATION/MicSpams/kinky_voice_input.wav "/home/$USER/.steam/steam/steamapps/common/$GAME/voice_input.wav"; echo "${BOLDFONT}$voiceinput${NORMALFONT} entered"; break;;
 		"Medic!")
 			cp $SCRIPTLOCATION/MicSpams/medic_voice_input.wav "/home/$USER/.steam/steam/steamapps/common/$GAME/voice_input.wav"; echo "${BOLDFONT}$voiceinput${NORMALFONT} entered"; break;;
 		"OOF")
 			cp $SCRIPTLOCATION/MicSpams/oof_voice_input.wav "/home/$USER/.steam/steam/steamapps/common/$GAME/voice_input.wav"; echo "${BOLDFONT}$voiceinput${NORMALFONT} entered"; break;;
 		"IM THE MOUNTAIN")
 			cp $SCRIPTLOCATION/MicSpams/stoned_jesus_im_the_mountain_voice_input.wav "/home/$USER/.steam/steam/steamapps/common/$GAME/voice_input.wav"; echo "${BOLDFONT}$voiceinput${NORMALFONT} entered"; break;;
+		"Virgin In House")
+			cp $SCRIPTLOCATION/MicSpams/22_yo_virgin_voice_input.wav "/home/$USER/.steam/steam/steamapps/common/$GAME/voice_input.wav"; echo "${BOLDFONT}$voiceinput${NORMALFONT} entered"; break;;
 		"Quit")
-			break;;
+			exit;;
 		*) echo "invalid option $REPLY";;
 	esac
 	done
